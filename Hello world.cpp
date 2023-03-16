b72c4c8454c812e571455a7427ca3f6e63121054
essaqur <ivan.stishenko@bk.ru>
2023-03-16 18:36:20 +0300

    Update Hello world.cpp

--- Hello world.cpp
+++ Hello world.cpp
@@ -4,6 +4,6 @@ int
 main()
 {
   std::string name;
-  std::cin >> name;                                 // Enter name
-  std::cout << "Hello world from " << name << endl; // Name cout
+  std::cin >> name;                                 // Ввод ывафыва юывапюаывюБЮ ю,,,
+  std::cout << "Hello world from " << name << endl; // Выsdfsadf имени
 }
